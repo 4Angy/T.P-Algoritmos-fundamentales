1da77f5af549e723338ef204f8601eedcb29e503
Ema Maltrat <pearl.jam.ema@gmail.com>
2024-09-30 20:26:15 -0300

    ejercicio 5 completo y funcionando

--- src/ejercicio5.java
+++ src/ejercicio5.java
@@ -5,6 +5,7 @@ public class Ejercicio5 {
     public static void Ej5(Scanner scanner){
         int filas,columnas;
         int promedio=0,resultado=0;
+        //Le pido al usuario la dimension de la matriz
         do{
          System.out.println("Ingrese un numero igual o mayor a 2");
          filas=scanner.nextInt();
@@ -14,6 +15,7 @@ public class Ejercicio5 {
          scanner.nextLine();
         }while ( filas<2 || columnas <2);
         int[][] arreglo = new int[filas][columnas];
+        //Lleno la matriz con los datos que de el usuario
         System.out.println("Ingrese los numeros del arreglo de "+filas+" x "+columnas);
         for(int i = 0;i < filas;i++){
             for(int j = 0;j < columnas;j++){
@@ -27,19 +29,28 @@ public class Ejercicio5 {
             }
          System.out.print("\n");
         }
+        //Sumo los elementos del arreglo
         for(int i = 0;i < filas;i++){
             for(int j = 0;j < columnas;j++){
              promedio+=arreglo[i][j];
             }
         }
+        //Saco el promedio del arreglo
         resultado=promedio/(filas*columnas);
-        System.out.println("El promedio de la matriz es : "+resultado);
+        System.out.println("El promedio entero es : "+resultado);
+        //Busco el numero en el arreglo
+        int coincidencia=0;
         for(int i = 0;i < filas;i++){
             for(int j = 0;j < columnas;j++){
                 if (arreglo[i][j]==resultado){
-                 System.out.println("El numero "+resultado+" se encuentra en la fila "+(i+1)+" ,columna "+(j+1));
+                 System.out.println("El valor "+resultado+" se encuentra en la posicion con indice ("+i+","+j+") o fila "+(i+1)+" ,columna "+(j+1));
+                 coincidencia++;
                 }
+
             }
+        }
+        if (coincidencia==0) {
+            System.out.println("No se encontro ninguna coincidencia");
         }
     }
 }
