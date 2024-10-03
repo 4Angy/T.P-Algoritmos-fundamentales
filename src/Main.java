1c185708b9b1d0fa2c0b9da4c7b9a3f5697d79aa
4angy <angyzalazar18@gmail.com>
2024-10-02 23:46:39 -0300

    Agregé ejercicio 2 y 3 listos y funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int ejercicio;
         do {
-            System.out.println("Ingrese la opcion:");
+            System.out.println("Ingrese la opción:");
             System.out.println(
-                    "1)Ejercicio 1 (). \n" +
-                            "2)Ejercicio 2 (). \n" +
-                            "3)Ejercicio 3 (). \n" +
-                            "4)Ejercicio 4 (). \n" +
-                            "5)Ejercicio 5 (). \n" +
-                            "0)Salir ");
+                    "1) Ejercicio 1 (No implementado). \n" +
+                            "2) Ejercicio 2 (Algoritmo de Inserción). \n" +
+                            "3) Ejercicio 3 (Algoritmo ASC DESC). \n" +
+                            "4) Ejercicio 4 (No implementado). \n" +
+                            "5) Ejercicio 5 (Ejercicio 5). \n" +
+                            "0) Salir ");
             ejercicio = scanner.nextInt();
             scanner.nextLine();
 
             switch (ejercicio) {
-
                 case 1:
                     break;
                 case 2:
+                    Ejercicio2.run(scanner);
                     break;
                 case 3:
+                    ejercicio3.run(scanner);
                     break;
                 case 4:
                     break;
@@ -30,12 +31,15 @@ public static void main(String[] args) {
                     Ejercicio5.Ej5(scanner);
                     break;
                 case 0:
+
                     break;
                 default:
-                    System.out.println("Esto no es un ejercicio, intente de nuevo");
+                    System.out.println("Opción no válida, intente de nuevo.");
                     break;
             }
 
         } while (ejercicio != 0);
+
+        scanner.close();
     }
 }
